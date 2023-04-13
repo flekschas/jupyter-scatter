7f6a49eea2cd70d08c73cb1642ffd422d0b16111
Fritz Lekschas <code@lekschas.de>
2023-04-12 23:50:09 -0400

    Bump to v0.12.0

--- jscatter/_version.py
+++ jscatter/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (0, 11, 0, 'final', 1)
+version_info = (0, 12, 0, 'final', 1)
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'alpha', 'beta': 'beta', 'candidate': 'rc', 'final': ''}
