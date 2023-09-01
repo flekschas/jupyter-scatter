eee1b1449f24492a2f074844affaa420acac3548
Fritz Lekschas <code@lekschas.de>
2023-09-01 13:50:24 -0400

    docs: add documentation site (#92)
    * Initial documentation website
    
    * Add social media teaser image

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,9 @@
+// .vitepress/theme/index.js
+import DefaultTheme from 'vitepress/theme';
+import Layout from './Layout.vue';
+import './custom.css';
+
+export default {
+  ...DefaultTheme,
+  Layout: Layout,
+};
