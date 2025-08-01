02732447bf45f5815601cb0a7c4b33df20f2d68b
Benoit Hamelin <benoit@benoithamelin.com>
2025-08-01 15:50:06 -0400

    Add robustness to importing extra dependencies
    Whenever an import fail, we silently define a mock object. Whenever this
    object is dereferenced and used, an exception indicating the missing
    extra is raised.

--- jscatter/dependencies.py
+++ jscatter/dependencies.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
+from enum import Enum
 from importlib.util import find_spec
-from typing import Optional
+from typing import Any, Optional
 
 
 class DependencyError(ImportError):
@@ -46,3 +48,66 @@ def check_label_extras_dependencies() -> None:
     """Check if label extras dependencies are installed."""
     check_optional_dependency('progress display', 'tqdm', 'label-extras')
     check_optional_dependency('largest_cluster positioning', 'hdbscan', 'label-extras')
+
+
+@dataclass
+class MissingPackage:
+    """
+    This is used as a stand-in object when importing dependencies that were not
+    installed, so as to keep the dependency profile slimmer. It duck-talks like
+    a module. If any function, class or object is queried out of it, it raises
+    an exception. Developers should be sure to guard such accesses with
+    properly positioned check_*_dependencies function calls.
+    """
+    name: str
+    extra: str
+
+    def __getattr__(self, name: str) -> Any:
+        raise DependencyError(
+            f'Attemping to query member {name} from module {self.name}. '
+            'However, this module was not installed as an optional dependency '
+            'to Jupyter Scatter. Please install it with either '
+            f'`pip install "jupyter-scatter[{self.extra}]"` '
+            'or `pip install "jupyter-scatter[all]".'
+        )
+
+
+class Action(Enum):
+    instantiate_class = "instantiate class"
+    call_function = "call function"
+
+
+@dataclass
+class MissingCallable:
+    action: Action
+    name: str
+    module: str
+    extra: str
+
+    def __call__(self, *args, **kwargs):
+        raise DependencyError(
+            f'Attempting to {self.action.value} {self.name} from '
+            f'module {self.module}. '
+            'However, this module was not installed as an optional dependency '
+            'to Jupyter Scatter. Please install it with either '
+            f'`pip install "jupyter-scatter[{self.extra}]"` '
+            'or `pip install "jupyter-scatter[all]".'
+        )
+
+    @classmethod
+    def class_(cls, name: str, module: str, extra: str) -> "MissingCallable":
+        return cls(
+            action=Action.instantiate_class,
+            name=name,
+            module=module,
+            extra=extra,
+        )
+
+    @classmethod
+    def function(cls, name: str, module: str, extra: str) -> "MissingCallable":
+        return cls(
+            action=Action.call_function,
+            name=name,
+            module=module,
+            extra=extra,
+        )
